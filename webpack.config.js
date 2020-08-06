f9c09800e9d4f302bdb0bc365664cc01bbfcded8
Sergei Liddell <liddell.s@pm.me>
2020-08-05 18:37:55 -0600

    Configures webpack config file
    * Adds entry points babel options and output.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 module.exports = {
+  entry: __dirname + 'ratings_reviews/client/src/index.jsx',
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: [/\.jsx$/],
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
         }
       }
     ]
+  },
+   output: {
+    filename: 'bundle.js',
+    path: __dirname + 'ratings_reviews/client/dist/index.html'
   }
 };
