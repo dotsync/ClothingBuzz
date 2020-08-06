aa7694bac5d185ba02e8ad18469bdda94f653a3a
Sergei Liddell <liddell.s@pm.me>
2020-08-06 11:52:33 -0600

    Creates first test
    * Checks whether the message I expect renders to the screen.
    * Passes

---
+++ client/src/components/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from 'enzyme';
+import App from './App';
+
+configure({ adapter: new Adapter() });
+
+test('Should contain the title Hello, World', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('#title').text()).toBe('Hello, World');
+});
