40f87c580534ac9ad840c0996b4e1951932056c7
Sergei Liddell <liddell.s@pm.me>
2020-08-06 11:27:52 -0600

    Removes component from index

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
 };
+
+// A recommended implementation below:
+/* module.exports = {
+  presets: [
+    '@babel/preset-react',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+}; */
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => <h1>Hello</h1>;
+// eslint-disable-next-line import/extensions
+import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
