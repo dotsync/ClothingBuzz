ee584c714c32bed2f1e57d82e02d6abad4c033fc
Sergei Liddell <liddell.s@pm.me>
2020-08-07 11:01:56 -0600

    Exports const data
    * Removes quotes because now all the data is inside of a data object.

--- client/src/__mock__/productTwo.js
+++ client/src/__mock__/productTwo.js
@@ -1,52 +1,54 @@
 /* List Reviews
-Returns a list of reviews for a particular product. This list does not include any reported reviews. GET /reviews/:product_id/list
+Returns a list of reviews for a particular product.
+This list does not include any reported reviews. GET /reviews/:product_id/list
 
 Parameters
 
-Parameter	Type	Description
-product_id	integer	Specifies the product for which to retrieve reviews.
-page	integer	Selects the page of results to return. Default 1.
-count	integer	Specifies how many results per page to return. Default 5.
-sort	text	Changes the sort order of reviews to be based on "newest", "helpful", or "relevant" */
+Parameter	        Type	                         Description
+product_id	      integer       	Specifies the product for which to retrieve reviews.
+page	            integer         Selects the page of results to return. Default 1.
+count	            integer	        Specifies how many results per page to return. Default 5.
+sort	            text	          Changes the sort order of reviews to be based on "newest", "helpful", or "relevant" */
 
-{
-  "product": "2",
-  "page": 0,
-  "count": 5,
-  "results": [
+const data = {
+  product: '2',
+  page: 0,
+  count: 5,
+  results: [
     {
-      "review_id": 5,
-      "rating": 3,
-      "summary": "I'm enjoying wearing these shades",
-      "recommend": 0,
-      "response": "",
-      "body": "Comfortable and practical.",
-      "date": "2019-04-14T00:00:00.000Z",
-      "reviewer_name": "shortandsweeet",
-      "helpfulness": 5,
-      "photos": [{
-          "id": 1,
-          "url": "urlplaceholder/review_5_photo_number_1.jpg"
-        },
-        {
-          "id": 2,
-          "url": "urlplaceholder/review_5_photo_number_2.jpg"
-        },
+      review_id: 5,
+      rating: 3,
+      summary: "I'm enjoying wearing these shades",
+      recommend: 0,
+      response: '',
+      body: 'Comfortable and practical.',
+      date: '2019-04-14T00:00:00.000Z',
+      reviewer_name: 'shortandsweeet',
+      helpfulness: 5,
+      photos: [{
+        id: 1,
+        url: 'urlplaceholder/review_5_photo_number_1.jpg',
+      },
+      {
+        id: 2,
+        url: 'urlplaceholder/review_5_photo_number_2.jpg',
+      },
         // ...
-      ]
+      ],
     },
     {
-      "review_id": 3,
-      "rating": 4,
-      "summary": "I am liking these glasses",
-      "recommend": 0,
-      "response": "Glad you're enjoying the product!",
-      "body": "They are very dark. But that's good because I'm in very sunny spots",
-      "date": "2019-06-23T00:00:00.000Z",
-      "reviewer_name": "bigbrotherbenjamin",
-      "helpfulness": 5,
-      "photos": [],
+      review_id: 3,
+      rating: 4,
+      summary: 'I am liking these glasses',
+      recommend: 0,
+      response: "Glad you're enjoying the product!",
+      body: "They are very dark. But that's good because I'm in very sunny spots",
+      date: '2019-06-23T00:00:00.000Z',
+      reviewer_name: 'bigbrotherbenjamin',
+      helpfulness: 5,
+      photos: [],
     },
     // ...
-  ]
+  ],
 }
+export default data;
