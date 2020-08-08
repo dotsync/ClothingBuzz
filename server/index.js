da94937c9d5c46aba0fbdebdb317ee55b92123b2
Sergei Liddell <liddell.s@pm.me>
2020-08-08 10:24:19 -0600

    Plans implementation of endpoints

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -9,10 +10,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(`${__dirname}/../client/dist`));
 
-// app.get('/api/blogs', async (req, res) => {
-// });
+// GET /reviews/:product_id/list -- returns a list of reviews for a particular product.
+// The above list does not get any reported reviews.
+
+// GET /reviews/:product_id/meta -- returns a list of meta data for a particular products review.
+
+// POST /reviews/:product_id -- adds a review for a given product.
+
+// PUT /reviews/helpful/:product_id -- updates a review to show it was helpful.
+
+// PUT /reviews/report/:review_id -- updates a review to show it was reported. This action does not
+// delete the review but the review will not be returned in the GET list request.
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`listening on port ${PORT}`);
 });
