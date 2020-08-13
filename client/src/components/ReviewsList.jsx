a03bd74e472a5cd2ff242526a83e6fb112529e7a
Sergei Liddell <liddell.s@pm.me>
2020-08-13 07:20:35 -0600

    Fixes bug where path name was wrong

--- client/src/components/ReviewsList.jsx
+++ client/src/components/ReviewsList.jsx
@@ -7,7 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Grid';
 
-import AddReviewButton from './AddreviewButton.jsx';
+import AddReviewButton from './AddReviewButton.jsx';
 import ReviewTile from './ReviewTile.jsx';
 import StarRating from './StarRating.jsx';
 import RatingsBreakdown from './RatingsBreakdown.jsx';
