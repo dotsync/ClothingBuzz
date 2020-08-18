fa4b7a08bf7b8b611341dc02dcd713893e390554
Sergei Liddell <liddell.s@pm.me>
2020-08-18 14:25:13 -0600

    Implements RatingsPercent component
    * Succesfully renders percentage of people that recommend the product.
    * If there are no yes votes this is refelected.

--- client/src/components/RatingsPercent.jsx
+++ client/src/components/RatingsPercent.jsx
@@ -6,19 +6,25 @@ export default function RatingsPercent(props) {
     ? productRecommended = props.productsMetaData.recommended
     : console.log('recommended is likely undefined... waiting to assign meta recommended');
 
-    // recommended:
-    // 0: 8
-    // 1: 9
-
-    // create a total
+  const getRecommendationPercent = (recommendations) => {
+  // create a total
+    let totalVotes = 0;
     // add keys to total if they exist else set value to null
+    recommendations[0] ? totalVotes += recommendations[0] : recommendations[0] = null;
+    recommendations[1] ? totalVotes += recommendations[1] : recommendations[1] = null;
     // divide the yes votes by the total then times it by 100 if yes votes exist
     // then return the string -> `${percentage}% of people recommend this product.`
+    if (recommendations[1] !== null) {
+      const avg = Math.floor((recommendations[1] / totalVotes) * 100);
+      return `${avg}% of people recommend this product`;
+    }
     // else return 0 percent of people recommend because yes votes dont exist
+    return '0% of people recommend this product';
+  };
 
   return (
     <div>
-{console.log('rec', productRecommended)}
+      {getRecommendationPercent(productRecommended)}
     </div>
   );
 }
