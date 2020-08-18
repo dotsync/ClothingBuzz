2bf3013b2e1f93c18bf4ca2cf8f84a88a48c4fd2
Sergei Liddell <liddell.s@pm.me>
2020-08-18 09:04:13 -0600

    Deletes wrong implementation of RatingsSize

--- client/src/components/RatingsLinearProgress.jsx
+++ client/src/components/RatingsLinearProgress.jsx
@@ -3,7 +3,9 @@ import { LinearProgress } from '@material-ui/core';
 
 export default function RatingsLinearProgress(props) {
   let ratings;
-  props.productsMetaData.ratings ? ratings = props.productsMetaData.ratings : console.log('waiting to assign ratings');
+  props.productsMetaData.ratings
+    ? ratings = props.productsMetaData.ratings
+    : console.log('ratings is likely undefined... waiting to assign meta ratings');
 
   const totalStars = Math.floor(Object.keys(ratings).reduce((sum, key) => sum + ratings[key], 0));
   const numbers = [5, 4, 3, 2, 1];
--- client/src/components/RatingsSize.jsx
+++ client/src/components/RatingsSize.jsx
@@ -1,46 +1,6 @@
 import React from 'react';
 
 export default function RatingsSize(props) {
-  let totalSize = 0;
-  let averageSize = 0;
-  const { reviews } = props;
-  // get average
-  reviews.map((item) => totalSize += item.rating);
-  averageSize = totalSize / reviews.length;
-
-  // star values
-  const sizeItems = {
-    five: 0,
-    four: 0,
-    three: 0,
-    two: 0,
-    one: 0,
-  };
-  // fill starItems
-  reviews.map((review) => (review.rating === 1 && sizeItems.one++));
-
-  // totals stars = 100%
-  // 100% divided by total stars give me my multiplyer.. floor it.
-  const multiplier = Math.ceil(100 / totalSize);
-  // current item times multiplier gives racalibrated amount
-  // console.log('multiplier', Math.floor(multiplier));
-
-  //   return (
-  //     <div>
-  //       <u>5 stars</u>
-  //       <LinearProgress variant="determinate" value={starItems.five * multiplier} />
-  //       <u>4 stars</u>
-  //       <LinearProgress variant="determinate" value={starItems.four * multiplier} />
-  //       <u>3 stars</u>
-  //       <LinearProgress variant="determinate" value={starItems.three * multiplier} />
-  //       <u>2 stars</u>
-  //       <LinearProgress variant="determinate" value={starItems.two * multiplier} />
-  //       <u>1 stars</u>
-  //       <LinearProgress variant="determinate" value={starItems.one * multiplier} />
-  //     </div>
-  //   )
-  // }
-
 
   return (
     <div>
